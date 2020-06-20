3052e2ceca3b4cba2f3c6c4d6c4b96a0b9c4e653
cuiyang <cuiyangmail@163.com>
2020-06-20 16:26:50 +0800

    移除activeMQ

--- src/main/java/priv/cy/owner/config/activemq/JmsConfiguration.java
+++ src/main/java/priv/cy/owner/config/activemq/JmsConfiguration.java
@@ -1,70 +1,58 @@
 package priv.cy.owner.config.activemq;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.jms.annotation.EnableJms;
-import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
-import org.springframework.jms.config.JmsListenerContainerFactory;
-import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
-import org.springframework.jms.support.converter.MessageConverter;
-import org.springframework.jms.support.converter.MessageType;
-
-import javax.jms.ConnectionFactory;
-
-
 /**
  * @author ：cuiyang
  * @description：同时支持queue和topic
  * @date ：Created in 2020/5/24 12:46
  */
-@Configuration
-@EnableJms
-public class JmsConfiguration {
-    /**
-     * topic模式的ListenerContainer
-     *
-     * @param activeMQConnectionFactory
-     * @return
-     */
-    @Bean
-    public JmsListenerContainerFactory<?> jmsListenerContainerTopic(ConnectionFactory activeMQConnectionFactory) {
-        DefaultJmsListenerContainerFactory bean = new DefaultJmsListenerContainerFactory();
-        bean.setPubSubDomain(true);
-        bean.setConnectionFactory(activeMQConnectionFactory);
-        /**
-         * 使用消息转换器
-         */
-        bean.setMessageConverter(jacksonJmsMessageConverter());
-        return bean;
-    }
-
-    /**
-     * queue模式的ListenerContainer
-     *
-     * @param activeMQConnectionFactory
-     * @return
-     */
-    @Bean
-    public JmsListenerContainerFactory<?> jmsListenerContainerQueue(ConnectionFactory activeMQConnectionFactory) {
-        DefaultJmsListenerContainerFactory bean = new DefaultJmsListenerContainerFactory();
-        bean.setConnectionFactory(activeMQConnectionFactory);
-        /**
-         * 使用消息转换器
-         */
-        bean.setMessageConverter(jacksonJmsMessageConverter());
-        return bean;
-    }
-
-    /**
-     * 消息转换器
-     *
-     * @return
-     */
-    @Bean
-    public MessageConverter jacksonJmsMessageConverter() {
-        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
-        converter.setTargetType(MessageType.TEXT);
-        converter.setTypeIdPropertyName("_type");
-        return converter;
-    }
-}
+//@Configuration
+//@EnableJms
+//public class JmsConfiguration {
+//    /**
+//     * topic模式的ListenerContainer
+//     *
+//     * @param activeMQConnectionFactory
+//     * @return
+//     */
+//    @Bean
+//    public JmsListenerContainerFactory<?> jmsListenerContainerTopic(ConnectionFactory activeMQConnectionFactory) {
+//        DefaultJmsListenerContainerFactory bean = new DefaultJmsListenerContainerFactory();
+//        bean.setPubSubDomain(true);
+//        bean.setConnectionFactory(activeMQConnectionFactory);
+//        /**
+//         * 使用消息转换器
+//         */
+//        bean.setMessageConverter(jacksonJmsMessageConverter());
+//        return bean;
+//    }
+//
+//    /**
+//     * queue模式的ListenerContainer
+//     *
+//     * @param activeMQConnectionFactory
+//     * @return
+//     */
+//    @Bean
+//    public JmsListenerContainerFactory<?> jmsListenerContainerQueue(ConnectionFactory activeMQConnectionFactory) {
+//        DefaultJmsListenerContainerFactory bean = new DefaultJmsListenerContainerFactory();
+//        bean.setConnectionFactory(activeMQConnectionFactory);
+//        /**
+//         * 使用消息转换器
+//         */
+//        bean.setMessageConverter(jacksonJmsMessageConverter());
+//        return bean;
+//    }
+//
+//    /**
+//     * 消息转换器
+//     *
+//     * @return
+//     */
+//    @Bean
+//    public MessageConverter jacksonJmsMessageConverter() {
+//        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+//        converter.setTargetType(MessageType.TEXT);
+//        converter.setTypeIdPropertyName("_type");
+//        return converter;
+//    }
+//}
